21b3d73dc10ff1f7f78fcee4e02d6e60e6e84aa6
christinchick <alexey@skyward-crimea.ru>
2022-02-05 20:50:41 +0300

    add

--- src/test/java/firstTask/SoftAssertionTest.java
+++ src/test/java/firstTask/SoftAssertionTest.java
@@ -1,8 +1,11 @@
 package firstTask;
 
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
@@ -10,12 +13,14 @@ import static com.codeborne.selenide.Selenide.$;
 public class SoftAssertionTest {
     @Test
     void homeWorkTwo (){
+        SelenideLogger.addListener("allure", new AllureSelenide());
 
-        Selenide.open("https://github.com/selenide/selenide");
-        $("#wiki-tab").click();
-        $(".Layout-sidebar").$(byText("Show 2 more pages…")).click();
-        $(".Layout-sidebar").$(byText("SoftAssertions")).shouldHave(visible);
-        $(".Layout-sidebar").$(byText("SoftAssertions")).click();
-        $(".markdown-body").$(byText("Using JUnit5 extend test class:")).shouldHave(visible);
+        Selenide.open("https://skyward-crimea.ru");
+        $(".elementor-button-wrapper").click();
+        $(".elementor-widget-wrap").shouldHave(visible);
+        $("#form-field-name").setValue("Кристина");
+        $("#form-field-email").setValue("+79780382764");
+        $("button[type='submit']").click();
+        $(".elementor-message").shouldHave(text("Сообщение успешно отправлено!"));
     }
 }
