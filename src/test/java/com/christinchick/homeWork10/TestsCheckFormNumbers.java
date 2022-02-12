68999dfd240785d605ab51f7ca45fee0679c093e
Christinchick <alexey@skyward-crimea.ru>
2022-02-12 19:40:07 +0300

    Add Allure

--- src/test/java/com/christinchick/homeWork10/TestsCheckFormNumbers.java
+++ src/test/java/com/christinchick/homeWork10/TestsCheckFormNumbers.java
@@ -1,19 +1,32 @@
-package firstTask;
+package com.christinchick.homeWork10;
 
+import com.christinchick.homeWork10.configuration.TestBase;
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Story;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Browsers.OPERA;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 
-public class SoftAssertionTest {
+public class TestsCheckFormNumbers extends TestBase{
+
     @Test
+    @Owner("Christinchick")
+    @Feature("OpenFormOnSkywardCrimeaRU")
+    @Story("Проверка перехода на форму обратной связи")
+    @DisplayName("Проверка обратной связи на сайте")
     void homeWorkTwo (){
+        Configuration.browser = OPERA;
         SelenideLogger.addListener("allure", new AllureSelenide());
+
 
         Selenide.open("https://skyward-crimea.ru");
         $(".elementor-button-wrapper").click();
@@ -24,3 +37,4 @@ public class SoftAssertionTest {
         $(".elementor-message").shouldHave(text("Сообщение успешно отправлено!"));
     }
 }
+
---
+++ src/test/java/com/christinchick/homeWork10/configuration/TestBase.java
@@ -0,0 +1,31 @@
+package com.christinchick.homeWork10.configuration;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
+
+
+public class TestBase {
+    @BeforeAll
+    static void setUp(){
+    DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+    Configuration.browserCapabilities = capabilities;
+}
+
+    @AfterEach
+    void addAttachments() {
+
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+        closeWebDriver();
+    }
+}
